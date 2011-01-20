83cd6305fcac6eb38581c2625f6269c3fc07425a
Kevin Russ <kruss@lear.com>
2011-01-20 13:41:15 +0100

    some notes

--- src/core/Logistic.java
+++ src/core/Logistic.java
@@ -1,5 +1,5 @@
 package core;
 /** generated file */
 public class Logistic {
-  public static final String BUILD_DATE = "Fri Jan 14 18:22:43 +0100 2011";
+  public static final String BUILD_DATE = "Thu Jan 20 13:36:44 +0100 2011";
 }
