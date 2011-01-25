0e019752a0937fd4d03d1922b4aeb25f13d0c139
Kevin Russ <kruss@lear.com>
2011-01-25 09:59:17 +0100

    initial timed trigger

--- src/core/Logistic.java
+++ src/core/Logistic.java
@@ -1,5 +1,5 @@
 package core;
 /** generated file */
 public class Logistic {
-  public static final String BUILD_DATE = "Thu Jan 20 19:29:19 +0100 2011";
+  public static final String BUILD_DATE = "Mon Jan 24 18:31:01 +0100 2011";
 }
