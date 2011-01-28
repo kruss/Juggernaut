546c051f0868b3ecfb0ced6dafeaef950dfda8f7
Kevin Russ <kruss@lear.com>
2011-01-28 16:40:07 +0100

    updated todos

--- src/core/Logistic.java
+++ src/core/Logistic.java
@@ -1,5 +1,5 @@
 package core;
 /** generated file */
 public class Logistic {
-  public static final String BUILD_DATE = "Fri Jan 28 16:21:41 +0100 2011";
+  public static final String BUILD_DATE = "Fri Jan 28 16:36:21 +0100 2011";
 }
