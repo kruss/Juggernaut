d28f4cfda2bf5429863ac2361032e3fb90b94522
Kevin Russ <kruss@lear.com>
2011-03-16 13:05:27 +0100

    version 0.5.0 - supporting system-properties

--- src/core/Logistic.java
+++ src/core/Logistic.java
@@ -1,5 +1,5 @@
 package core;
 /** generated file */
 public class Logistic {
-  public static final String BUILD_DATE = "Wed Mar 16 13:01:37 +0100 2011";
+  public static final String BUILD_DATE = "Wed Mar 16 13:04:27 +0100 2011";
 }
