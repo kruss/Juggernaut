f8d5875f39aafeff653e734127cd66d2747c476d
Kevin Russ <kruss@lear.com>
2011-03-29 16:16:17 +0200

    version 0.5.9 - improved clone / backup for configuration

--- src/core/Logistic.java
+++ src/core/Logistic.java
@@ -1,5 +1,5 @@
 package core;
 /** generated file */
 public class Logistic {
-  public static final String BUILD_DATE = "Tue Mar 29 16:08:08 +0200 2011";
+  public static final String BUILD_DATE = "Tue Mar 29 16:15:23 +0200 2011";
 }
