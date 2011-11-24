d3e4a89d76b74358002ba08fec715a8fb7532cb4
Kevin Russ <kruss@muc-20-1126.(none)>
2011-11-24 18:40:30 +0100

    added linux starter script

--- src/core/Logistic.java
+++ src/core/Logistic.java
@@ -1,5 +1,5 @@
 package core;
 /** generated file */
 public class Logistic {
-  public static final String BUILD_DATE = "2011-11-18 17:41:27 +0100";
+  public static final String BUILD_DATE = "2011-11-24 18:39:42 +0100";
 }
