a0918cd61d21bd57425f9b0e48c2b0c4a8386779
Kevin Russ <kruss@muc-20-1126.(none)>
2011-12-22 10:49:37 +0100

    licence added

--- src/core/Logistic.java
+++ src/core/Logistic.java
@@ -1,5 +1,5 @@
 package core;
 /** generated file */
 public class Logistic {
-  public static final String BUILD_DATE = "2011-11-24 18:39:42 +0100";
+  public static final String BUILD_DATE = "2011-12-22 10:43:57 +0100";
 }
